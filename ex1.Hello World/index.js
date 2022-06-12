7d59dc6d854d376f92e6af440e3de1ddf50ac036
ponsAoki <aokishogo20pk@gmail.com>
2022-06-12 13:18:14 +0900

    ex1

--- ex1.Hello World/index.js
+++ ex1.Hello World/index.js
@@ -2,19 +2,14 @@ $(function() {
     $('.btn').click(
         function() {
             $('.modal-wrapper').fadeIn(300)
-                // $('.modal-wrapper').slideDown(300).animate({ opacity: 1 }, { queue: false, duration: 500 })
         },
     );
-    // $('.btn').keydown(
-    //     function() {
-    //         $('.modal').slideDown()
-    //     }
-    // )
+
     $(document).click(function(e) {
         if ($(e.target).closest('.modal-close').length || !$(e.target).is('.btn')) {
             // ターゲット要素の外側をクリックした時の操作
             $('.modal-wrapper').fadeOut(300)
         }
-        // $('.modal-wrapper').slideUp()
     })
+
 })
